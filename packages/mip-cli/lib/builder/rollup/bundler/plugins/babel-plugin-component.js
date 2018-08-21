3c5e57ed0d0a029b30297e4902f3e8dbf9e73a25
clark-t <clarktanglei@163.com>
2018-08-21 20:47:57 +0800

    add babel plugin

---
+++ packages/mip-cli/lib/builder/rollup/bundler/plugins/babel-plugin-component.js
@@ -0,0 +1,31 @@
+/**
+ * @file babel-plugin-component.js
+ * @author clark-t (clarktanglei@163.com)
+ */
+
+const {isComponentPath} = require('../../../../utils/project-path')
+const {pathFormat} = require('../../../../utils/helper')
+const path = require('path')
+
+module.exports = function ({types: t}) {
+  return {
+    visitor: {
+      Program (nodePath, opts) {
+        if (!isComponentPath(opts.opts.basedir, this.file.opts.filename)) {
+          return
+        }
+
+        let basename = path.basename(this.file.opts.filename)
+        let name = path.basename(basename, path.extname(basename))
+        let dirname = path.dirname(this.file.opts.filename)
+
+        for (let i = 0; i < nodePath.node.body.length; i++) {
+          if (t.isExportDefaultDeclaration(nodoPath.node.body[i])) {
+            let exportPath = nodePath.get(`body.${i}`)
+            break;
+          }
+        }
+      }
+    }
+  }
+}
